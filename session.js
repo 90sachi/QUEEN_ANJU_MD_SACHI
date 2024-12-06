cca452326326078aef82e3409d9e1583e9a3aec6
90sachi <githabgithab2@gmail.com>
2024-12-06 08:33:46 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=qVd3mYTa#0kqHmMBZom-5OMESee8pD78ERnaPrLblmGYkii-5RrI",
 };
